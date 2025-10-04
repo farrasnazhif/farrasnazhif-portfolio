69c528200682d806d421ddbb270122ba6c087e57
farrasnazhif <farrasnazhif@gmail.com>
2025-10-04 19:02:08 +0700

    feat: about page base component

---
+++ src/components/About.js
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <div className="relative bg-gradient-to-t from-black/150 to-slate-700/20 font-sans flex justify-center items-center min-h-screen p-0 gap-16 overflow-hidden">
+      <h1>Hi hai I&apos;m FArras</h1>
+    </div>
+  );
+}
